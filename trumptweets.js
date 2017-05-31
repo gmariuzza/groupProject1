71d41625d0576fb69d25d2551d93bc97c333dff5
Allison O'Hanlon <allison.t.ohanlon@gmail.com>
2017-05-31 18:33:21 -0500

    tweet api

--- trumptweets.js
+++ trumptweets.js
@@ -3,6 +3,40 @@ console.log("www");
 var type = "";
 var randomTrump;
 var fakeNames = ["Barack Obama", "Hillary Clinton", "Jeb Bush", "James Comey"];
+var tweets = [];
+var searchTerm = ["Hillary", "Mexico", "Obama", "Money", "Immigrants"];
+var tweetArray;
+
+		for (var i = 0; i < searchTerm.length; i++) {
+		    $.ajax({
+		    url: "https://matchilling-tronald-dump-v1.p.mashape.com/search/quote?query=" + searchTerm[i] + "&page=The+page+number&size=25'",
+
+        	// The URL to the API. You can get this by clicking on "Show CURL example" from an API profile
+        	type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
+        	data: {}, // Additional parameters here
+        	dataType: 'json',
+        	success: function(data) {
+        	},
+        	error: function(err) { console.log("error", err); },
+        	beforeSend: function(xhr) {
+        	xhr.setRequestHeader("X-Mashape-Authorization", "Y3uBFDenHumshrBSTtJm0yC7jf9Sp1Ri5m2jsnCHgHAwZg3rgv"); // Enter here your Mashape key
+        	}
+			})
+ 			.done(function(data) {     
+		      	console.log(data);
+				for (var  i = 0; i < data.count; i++ ) {
+					var tweetArray = data._embedded.quotes[i].value;
+		      		if ("undefined" === typeof tweetArray) {
+		      			console.log("undefined");
+		      		}
+		      		else {
+		      			tweets.push(tweetArray);
+			      	}
+		      	} 
+
+			});
+  		  	
+ 		}
 
 function ranNumber(min, max) {
   	return Math.floor(Math.random() * (max - min)) + min;
@@ -56,18 +90,27 @@ function ranNumber(min, max) {
 		  	$("#quoteHere").html(response.message);
 
 		  });
+		};  
 
-		  
 
-	};
+		 function tweet() {
+		$("#quoteHere").empty();
 
+		type = "tweet";
+
+		var randomQuote = Math.floor((Math.random() * tweets.length) + 1);
+		console.log(tweets[randomQuote]);
+		$("#quoteHere").html(tweets[randomQuote]);  
+			
+		}
 
 
 $("#generate").on("click", function(event) {
 
 	event.preventDefault();
 
-	randomTrump = ranNumber(0,2);
+	randomTrump = ranNumber(0,3);
+	console.log(randomTrump);
 
 	if (randomTrump === 0){
 
@@ -77,7 +120,7 @@ $("#generate").on("click", function(event) {
 		quotes();
 	}
 	else{
-		console.log("trump rules");
+		tweet();
 	};
 
 });
